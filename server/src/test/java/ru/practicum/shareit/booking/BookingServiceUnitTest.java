633b0f5c9476c55803e64ce93a8125b824ed9a7b
desmoo00 <dimckalegend2@yandex.com>
2026-03-02 15:34:02 -0600

    Фикс неиспользуемых импортов

--- server/src/test/java/ru/practicum/shareit/booking/BookingServiceUnitTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingServiceUnitTest.java
@@ -22,10 +22,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
